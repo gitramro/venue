e50bd94eed7d345a95ca7f11bad535968ce5f81d
Ramon <ramon.osc93@gmail.com>
2018-10-31 21:49:19 -0700

    Navbar, Featured, VenueInfo, Highlights and reusable Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import './resources/styles.css';
 
+import Header from './Components/Header/Header_Footer/Header';
+import Featured from './Components/Featured';
+import VenueInfo from './Components/VenueInfo';
+import Highlight from './Components/HighLight';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      Hello
+      <div
+        className="App"
+        style={{ height: '1500px', background: 'cornflowerblue' }}
+      >
+        <Header />
+        <Featured />
+        <VenueInfo />
+        <Highlight />
       </div>
     );
   }
